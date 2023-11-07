9dcef05badfa0733ccbfe3d85b7edcc086a3758e
rafaeldfigueiredo <rafaeldfigueiredo@hotmail.com>
2023-11-07 14:46:17 +0000

    add page components

--- ActualProject/frontend/src/App.jsx
+++ ActualProject/frontend/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import CreateBooks from './pages/CreateBooks';
+import DeleteBooks from './pages/DeleteBooks';
+import EditBooks from './pages/EditBooks';
+import Home from './pages/Home';
+import ShowBooks from './pages/ShowBooks';
 
 const App = () => {
   return (
-    <React.Fragment> 
-    <div className='bg-red-400 text-white'>
-    Hello World
-    </div>
-    </React.Fragment>
-  )
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='' element={<ShowBooks/>}/>
+    <Route path='' element={<CreateBooks/>}/>
+    <Route path='' element={<EditBooks/>}/>
+    <Route path='' element={<DeleteBooks/>}/>
+    </Routes>
+)
 }
 
 export default App
